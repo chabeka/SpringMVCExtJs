71d831453761d049da9ad365c15c098462dcae59
Aliou Diallo <aliou@adiallo.local>
2017-07-13 17:54:08 +0200

    rien

--- gjournaux/src/main/java/com/gj/manager/IBswJournalManager.java
+++ gjournaux/src/main/java/com/gj/manager/IBswJournalManager.java
@@ -1,46 +1,17 @@
 package com.gj.manager;
 
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.gj.dao.IBswJournalDAO;
 import com.gj.model.BswJournal;
 
-@Service
-public class BswJournalManager {
+public interface IBswJournalManager {
 
-	@Autowired
-	IBswJournalDAO bswJournalDAO;
-	
-	
-	public BswJournalManager() {
 
-	}
+	public List<BswJournal> allBswJournal();
 	
-	@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
-	public List<BswJournal> allBswJournal(){
-		List<BswJournal> listJournal = (List<BswJournal>) bswJournalDAO.getAll();
-		return listJournal;	
-	} 
-	
-	@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
-	public List<BswJournal> listBswJournal(String name){
-		List<BswJournal> listJournal = (List<BswJournal>) bswJournalDAO.getByName(name);
-		return listJournal;	
-	}
+	public List<BswJournal> listBswJournal(String name);
 
-	@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
-	public BswJournal getJournalById(Integer idJournal){
-		return bswJournalDAO.getByIdJournal(idJournal);
-	}
+	public BswJournal getJournalById(Integer idJournal);
 	
-	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
-	public BswJournal addBswJournal(BswJournal journal){
-		return bswJournalDAO.addBswJournal(journal);
-	}
+	public BswJournal addBswJournal(BswJournal journal);
 }
